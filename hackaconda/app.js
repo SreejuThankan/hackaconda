3165fed9ca9128d6d9292d8d69901eab182c588c
alexanderbuck <alex.buck10@gmail.com>
2014-08-16 21:59:14 +0100

    refactoring

--- hackaconda/app.js
+++ hackaconda/app.js
@@ -4,44 +4,45 @@ var io = require('socket.io')(http);
 
 var playersCreated = 0;
 
-var player1 = {
-    id:1,
-    x: 0,
-    y: 0,
-    moveUp : function() {
-        --this.y;
+var players =
+{
+     player1 : {
+        id: 1,
+        x : 50,
+        y : 0,
+        moveUp: function () {
+            --this.y;
+        },
+        moveDown: function () {
+            ++this.y;
+        },
+        moveRight: function () {
+            ++this.x;
+        },
+        moveLeft: function () {
+            --this.x;
+        }
+
     },
-    moveDown: function() {
-        ++this.y;
-    },
-    moveRight: function() {
-        ++this.x;
-    },
-    moveLeft:function(){
-        --this.x;
+ player2: {
+        id: 2,
+        x: 0,
+        y: 0,
+        moveUp: function () {
+            --this.y;
+        },
+        moveDown: function () {
+            ++this.y;
+        },
+        moveRight: function () {
+            ++this.x;
+        },
+        moveLeft: function () {
+            --this.x;
+        }
+
     }
-
 };
-
-var player2 = {
-    id:2,
-    x: 0,
-    y: 0,
-    moveUp : function() {
-        --this.y;
-    },
-    moveDown: function() {
-        ++this.y;
-    },
-    moveRight: function() {
-        ++this.x;
-    },
-    moveLeft:function(){
-        --this.x;
-    }
-
-};
-
 app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
@@ -50,33 +51,21 @@ app.get('/playerId', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-    console.log('a user connected with id :' + playersCreated);
+    console.log('a user connected');
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
-    socket.on('movePlayer1Up', function (direction) {
-        player1.moveUp();
+    socket.on('movePlayerLeft', function (player) {
+        players["player" + player.id].moveLeft();
     });
-    socket.on('movePlayer1Down', function (direction) {
-        player1.moveDown();
+    socket.on('movePlayerRight', function (player   ) {
+        players["player" + player.id].moveRight();
     });
-    socket.on('movePlayer1Left', function (direction) {
-        player1.moveLeft();
+    socket.on('movePlayerUp', function (player) {
+        players["player" + player.id].moveUp();
     });
-    socket.on('movePlayer1Right', function (direction) {
-        player1.moveRight();
-    });
-    socket.on('movePlayer2Up', function (direction) {
-        player2.moveUp();
-    });
-    socket.on('movePlayer2Down', function (direction) {
-        player2.moveDown();
-    });
-    socket.on('movePlayer2Left', function (direction) {
-        player2.moveLeft();
-    });
-    socket.on('movePlayer2Right', function (direction) {
-        player2.moveRight();
+    socket.on('movePlayerDown', function (player) {
+        players["player" + player.id].moveDown();
     });
     setInterval(emitPlayerPositions, 20);
 });
@@ -86,5 +75,5 @@ http.listen(3000, function () {
 });
 
 function emitPlayerPositions() {
-    io.emit('update position', {player1: player1, player2:player2});
+    io.emit('update position', players);
 }
