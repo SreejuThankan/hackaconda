15156979534076499172f7123d51903cc96394d1
alexanderbuck <alex.buck10@gmail.com>
2014-08-16 14:40:57 +0100

    created seperate server and web projects

--- hackaconda/app.js
+++ hackaconda/app.js
@@ -2,23 +2,52 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+var player1 = {
+    id:1,
+    x: 0,
+    y: 0,
+    moveUp : function() {
+        --this.y;
+    },
+    moveDown: function() {
+        ++this.y;
+    },
+    moveRight: function() {
+        ++this.x;
+    },
+    moveLeft:function(){
+        --this.x;
+    }
+
+};
+
+app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-        console.log('message: ' + msg);
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
     });
+    socket.on('movePlayer1Up', function (direction) {
+        player1.moveUp();
+    });
+    socket.on('movePlayer1Down', function (direction) {
+        player1.moveDown();
+    });
+    socket.on('movePlayer1Left', function (direction) {
+        player1.moveLeft();
+    });
+    socket.on('movePlayer1Right', function (direction) {
+        player1.moveRight();
+    });
+    setInterval(emitPlayerPositions, 20);
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
     console.log('listening on *:3000');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
-});
+function emitPlayerPositions() {
+    io.emit('update position', {player1: player1});
+}
